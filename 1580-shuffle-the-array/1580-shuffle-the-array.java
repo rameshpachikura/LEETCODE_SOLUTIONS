47928f6b2476ce3bab78b4fc8dc1c10ca3b76d89
rameshpachikura <119393676+rameshpachikura@users.noreply.github.com>
2025-06-25 19:20:33 +0530

    Time: 0 ms (100%), Space: 44.9 MB (41.33%) - LeetHub

--- 1580-shuffle-the-array/1580-shuffle-the-array.java
+++ 1580-shuffle-the-array/1580-shuffle-the-array.java
@@ -2,19 +2,11 @@ class Solution {
     public int[] shuffle(int[] nums, int n) {
        int x=nums.length;
        int[] a=new int[x];
-       int y[]=new int[n];
-       int[] z=new int[n];
-       for(int i=0;i<n;i++){
-           y[i]=nums[i];
-       }
-       for(int i=0;i<n;i++){
-           z[i]=nums[n+i];
-       }
        int c=0;
        int d=1;
        for(int i=0;i<n;i++){
-          a[c]=y[i];
-          a[d]=z[i];
+          a[c]=nums[i];
+          a[d]=nums[(n+i)];
           d=d+2;
           c=c+2;
        }
