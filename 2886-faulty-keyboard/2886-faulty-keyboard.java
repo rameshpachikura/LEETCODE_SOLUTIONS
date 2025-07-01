8949081836f830ee49cda068c369068a65671b9f
rameshpachikura <119393676+rameshpachikura@users.noreply.github.com>
2025-07-01 09:57:10 +0530

    Time: 3 ms (95.22%), Space: 45.1 MB (34.98%) - LeetHub

--- 2886-faulty-keyboard/2886-faulty-keyboard.java
+++ 2886-faulty-keyboard/2886-faulty-keyboard.java
@@ -2,12 +2,10 @@ class Solution {
     public String finalString(String s) {
        int n=s.length();
        StringBuilder sb=new StringBuilder();
-       int x=0;
        for(int i=0;i<n;i++){
         char ch=s.charAt(i);
         if(ch=='i'){
             sb.reverse();
-            x=i;
         }
         else{
             sb.append(ch);
