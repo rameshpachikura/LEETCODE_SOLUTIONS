124d1b5ad8261567d6bd0f98ae4eb3cd14728d95
rameshpachikura <119393676+rameshpachikura@users.noreply.github.com>
2025-07-04 20:55:25 +0530

    Time: 1053 ms (93.16%), Space: 0B (100%) - LeetHub

--- 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
+++ 1509-replace-employee-id-with-the-unique-identifier/1509-replace-employee-id-with-the-unique-identifier.sql
@@ -1,1 +1,1 @@
-select e1.unique_id,e2.name from Employees as e2 left join EmployeeUNI as e1 on e1.id=e2.id;
+select e1.unique_id,e2.name from Employees  e2 left join EmployeeUNI  e1 on e1.id=e2.id;
