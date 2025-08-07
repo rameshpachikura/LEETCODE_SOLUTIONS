1eb7b2eb52b97e48921ba78bbf58e7fe3668efad
rameshpachikura <119393676+rameshpachikura@users.noreply.github.com>
2025-08-07 21:04:26 +0530

    Time: 44 ms (5.5%), Space: 45.6 MB (38%) - LeetHub

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/1437-minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,29 +1,25 @@
 class Solution {
-    public int minInsertions(String s) {
-      int n=s.length();
-      StringBuilder sb=new StringBuilder(s);
-      int[][] dp=new int[n][n];
-      for(int[] r:dp){
-        Arrays.fill(r,-1);
-      }
-      int ans=helper(n-1,n-1,s,sb.reverse().toString(),dp);
-      System.out.println(ans);
-      return n-ans;  
+    public int f(int i, int j, String s1, String s2,int[][] dp){
+        if(i<0 || j<0) return 0;
+        if(dp[i][j]!=-1) return dp[i][j];
+        int ans = 0;
+        if(s1.charAt(i)!=s2.charAt(j)) 
+            ans = 0 + Math.max(f(i-1,j,s1,s2,dp),f(i,j-1,s1,s2,dp));
+        else ans = 1 + f(i-1,j-1,s1,s2,dp);
+        return dp[i][j] = ans;
     }
-    public int helper(int i,int j,String s1,String s2,int[][] dp){
-        if(i<0 || j<0){
-            return 0;
-        }
-        int ans=0;
-        if(dp[i][j]!=-1){
-            return dp[i][j];
-        }
-        if(s1.charAt(i)==s2.charAt(j)){
-            dp[i][j]=1+helper(i-1,j-1,s1,s2,dp);
-        }
-        else{
-            dp[i][j]=Math.max(helper(i,j-1,s1,s2,dp),helper(i-1,j,s1,s2,dp));
-        }
-        return dp[i][j];
+    public int lcs(String text1, String text2) {
+        int i = text1.length() - 1, j = text2.length() - 1;
+        int dp[][] = new int[i+1][j+1];
+        for(int row[]:dp) Arrays.fill(row,-1);
+        return f(i,j,text1,text2,dp);
+    }
+    public int lps(String s) {
+        String rev = new StringBuilder(s).reverse().toString();
+        return lcs(s,rev);
+    }
+    public int minInsertions(String s) {
+        int n = s.length();
+        return n-lps(s);
     }
 }
