1cd96113fea4de7533bea083649cd61ed81b43d8
rameshpachikura <119393676+rameshpachikura@users.noreply.github.com>
2025-09-11 20:26:25 +0530

    Time: 0 ms (100%), Space: 40.3 MB (97.52%) - LeetHub

--- 0202-happy-number/0202-happy-number.java
+++ 0202-happy-number/0202-happy-number.java
@@ -1,21 +1,17 @@
 class Solution {
     public boolean isHappy(int n) {
-      Set<Integer> seen = new HashSet<>();
-        
-        while (n != 1 && !seen.contains(n)) {
-            seen.add(n);
-            n = helper(n);
+        while (n != 1 && n != 4) {
+            n = digitSquareSum(n);
         }
-        
         return n == 1;
-
     }
-    public int helper(int x){
-        int sum=0;
-        while(x>0){
-            int r=x%10;
-            sum+=r*r;
-            x=x/10;
+
+    private int digitSquareSum(int n) {
+        int sum = 0;
+        while (n > 0) {
+            int r = n % 10;
+            sum += r * r;
+            n /= 10;
         }
         return sum;
     }
