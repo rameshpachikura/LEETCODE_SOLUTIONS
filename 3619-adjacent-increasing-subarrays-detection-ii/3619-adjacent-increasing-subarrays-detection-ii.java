b35f7f2cfb915cbeec93ce3c7c3d935a081bad9c
rameshpachikura <119393676+rameshpachikura@users.noreply.github.com>
2025-10-15 22:29:39 +0530

    Time: 166 ms (7.45%), Space: 75.8 MB (75.53%) - LeetHub

--- 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
@@ -1,14 +1,26 @@
-
 class Solution {
+    public boolean possible(List<Integer>nums,int k){
+        int n=nums.size(),countinc=0;
+        int arr[]=new int[n];
+        for(int i=1;i<n;i++){
+            if(nums.get(i-1)<nums.get(i)) countinc++;
+             arr[i]=countinc;
+           if(i-2*k+1>=0) if(arr[i]-arr[i-k+1]==(k-1)&&arr[i-k]-arr[i-2*k+1]==(k-1)) return true;
+        }
+        
+        return false;
+    }
     public int maxIncreasingSubarrays(List<Integer> nums) {
-        int ans=0,prev=0,count=1;
-        for(int i=1;i<nums.size();i++){
-            if(nums.get(i)>nums.get(i-1)) count++;
-            else{
-                prev=count;
-                count=1;
+        int n=nums.size();
+        int low=1,high=n-1;
+        int ans=0;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(possible(nums,mid)){
+                ans=mid;
+                low=mid+1;
             }
-            ans=Math.max(ans,Math.max(count/2,Math.min(prev,count)));
+            else high=mid-1;
         }
         return ans;
     }
