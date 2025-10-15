ad407cd5caf88e501d9bb20ba054a6f90ae97968
rameshpachikura <119393676+rameshpachikura@users.noreply.github.com>
2025-10-15 22:28:19 +0530

    Time: 17 ms (64.89%), Space: 89.1 MB (29.79%) - LeetHub

---
+++ 3619-adjacent-increasing-subarrays-detection-ii/3619-adjacent-increasing-subarrays-detection-ii.java
@@ -0,0 +1,15 @@
+
+class Solution {
+    public int maxIncreasingSubarrays(List<Integer> nums) {
+        int ans=0,prev=0,count=1;
+        for(int i=1;i<nums.size();i++){
+            if(nums.get(i)>nums.get(i-1)) count++;
+            else{
+                prev=count;
+                count=1;
+            }
+            ans=Math.max(ans,Math.max(count/2,Math.min(prev,count)));
+        }
+        return ans;
+    }
+}
