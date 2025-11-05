50cda6f4275883b907ebd182d08533c65e67e6fc
rameshpachikura <119393676+rameshpachikura@users.noreply.github.com>
2025-11-05 10:15:55 +0530

    Time: 1340 ms (6.25%), Space: 195.8 MB (6.25%) - LeetHub

--- 3592-find-x-sum-of-all-k-long-subarrays-ii/3592-find-x-sum-of-all-k-long-subarrays-ii.java
+++ 3592-find-x-sum-of-all-k-long-subarrays-ii/3592-find-x-sum-of-all-k-long-subarrays-ii.java
@@ -1,88 +1,53 @@
-import java.util.*;
-
 class Solution {
-    private static final class Node {
-        final int freq, val;
-        Node(int freq, int val){ this.freq = freq; this.val = val; }
-        @Override public boolean equals(Object o){
-            if (this == o) return true;
-            if (!(o instanceof Node)) return false;
-            Node n = (Node)o; return freq == n.freq && val == n.val;
-        }
-        @Override public int hashCode(){ return Objects.hash(freq, val); }
+  public long[] findXSum(int[] nums, int k, int x) {
+    long[] ans = new long[nums.length - k + 1];
+    Map<Integer, Integer> count = new HashMap<>();
+    TreeSet<Pair<Integer, Integer>> top =
+        new TreeSet<>(Comparator.comparingInt(Pair<Integer, Integer>::getKey)
+                          .thenComparingInt(Pair<Integer, Integer>::getValue));
+    TreeSet<Pair<Integer, Integer>> bot =
+        new TreeSet<>(Comparator.comparingInt(Pair<Integer, Integer>::getKey)
+                          .thenComparingInt(Pair<Integer, Integer>::getValue));
+
+    for (int i = 0; i < nums.length; ++i) {
+      update(nums[i], 1, count, top, bot);
+      if (i >= k)
+        update(nums[i - k], -1, count, top, bot);
+      while (!bot.isEmpty() && top.size() < x) {
+        Pair<Integer, Integer> pair = bot.pollLast();
+        top.add(pair);
+        windowSum += (long) pair.getValue() * pair.getKey();
+      }
+      while (!bot.isEmpty() && (bot.last().getKey() > top.first().getKey() ||
+                                bot.last().getKey().equals(top.first().getKey()) &&
+                                    bot.last().getValue() > top.first().getValue())) {
+        Pair<Integer, Integer> pairB = bot.pollLast();
+        Pair<Integer, Integer> pairT = top.pollFirst();
+        top.add(pairB);
+        bot.add(pairT);
+        windowSum += (long) pairB.getValue() * pairB.getKey();
+        windowSum -= (long) pairT.getValue() * pairT.getKey();
+      }
+      if (i >= k - 1)
+        ans[i - k + 1] = windowSum;
     }
+    return ans;
+  }
 
-    private static final Comparator<Node> COMP = (a, b) -> {
-        if (a.freq != b.freq) return Integer.compare(b.freq, a.freq);
-        return Integer.compare(b.val, a.val);
-    };
+  private long windowSum = 0;
 
-    private Map<Integer,Integer> freqMap;
-    private TreeSet<Node> topSet, restSet;
-    private long topSum;
-
-    private void removeNode(int val, int freq){
-        Node key = new Node(freq, val);
-        if (topSet.remove(key)) {
-            topSum -= 1L * freq * val;
-        } else {
-            restSet.remove(key);
-        }
+  private void update(int num, int freq, Map<Integer, Integer> count,
+                      TreeSet<Pair<Integer, Integer>> top, TreeSet<Pair<Integer, Integer>> bot) {
+    if (count.getOrDefault(num, 0) > 0) { 
+      Pair<Integer, Integer> pair = new Pair<>(count.get(num), num);
+      if (bot.remove(pair)) {
+      } else {
+        top.remove(pair);
+        windowSum -= (long) num * count.get(num);
+      }
     }
-
-    private void addToTop(int val, int freq){
-        topSet.add(new Node(freq, val));
-        topSum += 1L * freq * val;
-    }
-
-    private void addValue(int val, int limit){
-        int freq = freqMap.getOrDefault(val, 0);
-        if (freq > 0) removeNode(val, freq);
-        freq += 1;
-        freqMap.put(val, freq);
-        addToTop(val, freq);
-        if (topSet.size() > limit){
-            Node lowest = topSet.last();
-            topSet.remove(lowest);
-            topSum -= 1L * lowest.freq * lowest.val;
-            restSet.add(lowest);
-        }
-    }
-
-    private void removeValue(int val, int limit){
-        Integer f = freqMap.get(val);
-        if (f == null || f == 0) return;
-        int freq = f;
-        removeNode(val, freq);
-        freq -= 1;
-        if (freq == 0) freqMap.remove(val);
-        else {
-            freqMap.put(val, freq);
-            restSet.add(new Node(freq, val));
-        }
-        if (topSet.size() < limit && !restSet.isEmpty()){
-            Node best = restSet.first();
-            restSet.remove(best);
-            topSet.add(best);
-            topSum += 1L * best.freq * best.val;
-        }
-    }
-
-    public long[] findXSum(int[] nums, int k, int x) {
-        int n = nums.length;
-        long[] ans = new long[n - k + 1];
-        freqMap = new HashMap<>(Math.max(16, n * 2));
-        topSet = new TreeSet<>(COMP);
-        restSet = new TreeSet<>(COMP);
-        topSum = 0;
-
-        for (int i = 0; i < k; ++i) addValue(nums[i], x);
-        ans[0] = topSum;
-        for (int i = k; i < n; ++i){
-            removeValue(nums[i - k], x);
-            addValue(nums[i], x);
-            ans[i - k + 1] = topSum;
-        }
-        return ans;
-    }
+    count.merge(num, freq, Integer::sum);
+    if (count.get(num) > 0)
+      bot.add(new Pair<>(count.get(num), num));
+  }
 }
